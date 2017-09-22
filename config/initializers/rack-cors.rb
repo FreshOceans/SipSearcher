2ac459e54076dba7f4f2df7514ac841041da2442
FreshOceans <mperezz398@gmail.com>
2017-09-22 12:47:27 -0400

    fixed rack-cors origins

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -2,12 +2,7 @@ if defined? Rack::Cors
   Rails.configuration.middleware.insert_before 0, Rack::Cors do
     allow do
       origins %w[
-        https://theconf.club
-        http://theconf.club
-        https://www.theconf.club
-        http://www.theconf.club
-        https://theconf.herokuapp.com
-        http://theconf.herokuapp.com
+        https://sipsearcher.herokuapp.com/
       ]
       resource '/assets/*'
     end
